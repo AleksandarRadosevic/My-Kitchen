9601ec4b568143479de357da0f8464e84b074fed
AleksandarRadosevic <aradosevic40@gmail.com>
2021-06-25 19:49:35 +0200

    Sortiranje po tezini

--- js/recipes.js
+++ js/recipes.js
@@ -1,4 +1,5 @@
 
+var recipesShow=[];
 $(document).ready(function(){
     let recipes=JSON.parse(localStorage.getItem("recipes"));
     function showRecipes(recipes){
@@ -24,9 +25,9 @@ $(document).ready(function(){
             else if (recipes[i].type==4){
                 foodType="snack";    
             }
-        
-        $("#pictures").append($("<div id='"+recipes[i].id+"' class='col-lg-4 col-md-6 special-grid recipes "+foodType+"'><div class='gallery-single fix'><img src='"+picture+"' class=img-fluid' style='width:100%; height:200px;' alt='Image'><div class='why-text'><h4>"+recipes[i].name+"</h4><p>Vreme pripreme recepta</p><h5>"+recipes[i].hour+":"+recipes[i].minute+"</h5></div></div></div>"));
-        //alert(foodType);
+        let rec=$("<div id='"+recipes[i].id+"' class='col-lg-4 col-md-6 special-grid recipes "+foodType+"'><div class='gallery-single fix'><img src='"+picture+"' class=img-fluid' style='width:100%; height:200px;' alt='Image'><div class='why-text'><h4>"+recipes[i].name+"</h4><p>Vreme pripreme recepta</p><h5>"+recipes[i].hour+":"+recipes[i].minute+"</h5></div></div></div>");
+        recipesShow.push(rec);
+        $("#pictures").append(rec);      
         }
       
     }
@@ -39,20 +40,108 @@ $(document).ready(function(){
         window.location.href="blog-details.html";
 
     });
-    function compareDiff( a, b ) {
+    function DiffAsc( a, b ) {
         if ( parseInt(a.difficulty) < parseInt(b.difficulty)){
-          return -1;
+            {             
+                var v1 = $('#'+a.id).html(),
+                v2 = $('#'+b.id).html();
+                $('#'+a.id).html(v2);
+                $('#'+b.id).html(v1);
+                $('#'+a.id).prop("id", "TEMP");
+                $('#'+b.id).prop("id", ""+a.id);
+                $('#TEMP').prop("id", ""+b.id); 
+                return -1;
+            }
+       
         }
-        if (parseInt(a.difficulty) < parseInt(b.difficulty)){
+        if (parseInt(a.difficulty) > parseInt(b.difficulty)){          
+          return 1;
+        }
+        return 0;
+      }
+      function DiffDesc( a, b ) {
+        if ( parseInt(a.difficulty) > parseInt(b.difficulty)){
+            {            
+            var v1 = $('#'+a.id).html(),
+            v2 = $('#'+b.id).html();
+            $('#'+a.id).html(v2);
+            $('#'+b.id).html(v1);
+            $('#'+a.id).prop("id", "TEMP");
+            $('#'+b.id).prop("id", ""+a.id);
+            $('#TEMP').prop("id", ""+b.id);                   
+                return -1;
+            }
+       
+        }
+        if (parseInt(a.difficulty) < parseInt(b.difficulty)){   
+                
           return 1;
         }
         return 0;
       }
     
       $(document).on("change","select",function(){
-        recipes.sort(compareDiff);
-        $("#pictures").remove();
-        $("#pics").append($("<div id='pictures'></div>"));
-        show(recipes);
+          if ($(this).val()==1)    
+        for (let i=0;i<recipes.length;i++)
+            for (let k=i+1;k<recipes.length;k++){
+                if (parseInt(recipes[i].difficulty)<parseInt(recipes[k].difficulty)){
+                    let a=recipes[i];
+                    let b=recipes[k];
+                    var v1 = $('#'+a.id).html(),
+                    v2 = $('#'+b.id).html();
+                    $('#'+a.id).html(v2);
+                    $('#'+b.id).html(v1);
+                    $('#'+a.id).prop("id", "TEMP");
+                    $('#'+b.id).prop("id", ""+a.id);
+                    $('#TEMP').prop("id", ""+b.id);        
+                    let temp=recipes[i];
+                    recipes[i]=recipes[k];
+                    recipes[k]=temp;
+                }
+            }
+            else if ($(this).val()==2)
+            for (let i=0;i<recipes.length;i++)
+            for (let k=i+1;k<recipes.length;k++){
+                if (parseInt(recipes[i].difficulty)>parseInt(recipes[k].difficulty)){
+                    let a=recipes[i];
+                    let b=recipes[k];
+                    var v1 = $('#'+a.id).html(),
+                    v2 = $('#'+b.id).html();
+                    $('#'+a.id).html(v2);
+                    $('#'+b.id).html(v1);
+                    $('#'+a.id).prop("id", "TEMP");
+                    $('#'+b.id).prop("id", ""+a.id);
+                    $('#TEMP').prop("id", ""+b.id);        
+                    let temp=recipes[i];
+                    recipes[i]=recipes[k];
+                    recipes[k]=temp;
+                }
+            }
+            else if ($(this).val()==3 || $(this).val()==4){
+                
+                for (let i=0; i<recipes.length;i++){
+
+                }
+            
+            for (let i=0;i<recipes.length;i++)
+            for (let k=i+1;k<recipes.length;k++){
+                if (parseInt(recipes[i].mark)<parseInt(recipes[k].difficulty)){
+                    let a=recipes[i];
+                    let b=recipes[k];
+                    var v1 = $('#'+a.id).html(),
+                    v2 = $('#'+b.id).html();
+                    $('#'+a.id).html(v2);
+                    $('#'+b.id).html(v1);
+                    $('#'+a.id).prop("id", "TEMP");
+                    $('#'+b.id).prop("id", ""+a.id);
+                    $('#TEMP').prop("id", ""+b.id);        
+                    let temp=recipes[i];
+                    recipes[i]=recipes[k];
+                    recipes[k]=temp;
+                }
+            }
+        }
+        localStorage.setItem("recipes",JSON.stringify(recipes));
     });
+     
 })
